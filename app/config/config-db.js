88cb8e4fb0e5a719e7ec12d5b95e834fb9e252ca
amaliaputlh <amaliaputh@gmail.com>
2024-05-28 18:52:34 +0700

    database change

--- app/config/config-db.js
+++ app/config/config-db.js
@@ -1,7 +1,7 @@
 module.exports = {
-  HOST: "localhost",
+  HOST: "34.30.85.108",
   USER: "postgres",
-  PASSWORD: "password",
+  PASSWORD: "152003",
   DB: "learnnode",
   dialect: "postgres",
   pool: {
